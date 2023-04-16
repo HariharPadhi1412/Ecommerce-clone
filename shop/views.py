bd02e79d23cefc08b807371871f5776096fa8f46
haripadhi08 <haripadhi08@gmail.com>
2023-04-17 00:29:33 +0530

    Added various html files for shop page and change the navbar with cart popup & and also displays the total number of individual item stored

--- shop/views.py
+++ shop/views.py
@@ -26,16 +26,16 @@ def about(request):
     return render(request,"shop/about.html")
 
 def checkout(request):
-    return render(request,"")
+    return render(request,"shop/checkout.html")
 
 def tracker(request):
-    return render(request,"")
+    return render(request,"shop/tracker.html")
 
 def search(request):
-    return render(request,"")
+    return render(request,"shop/search.html")
 
 def contact(request):
-    return render(request,"")
+    return render(request,"shop/contact.html")
 
 def prodView(request):
-    return render(request,"")
+    return render(request,"shop/prodView.html")
