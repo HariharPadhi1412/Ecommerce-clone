bdb39e6cd24cdff45017ecfa60a2c5b560f1ff5d
haripadhi08 <haripadhi08@gmail.com>
2023-04-16 09:01:09 +0530

    Added a SlideShow to the page with images and data pulling from the admin panel

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from . models import Product
+from math import ceil
 # from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request,'shop/index.html')
+    products = Product.objects.all()
+    product_length = len(products)
+
+    no_of_slides = product_length// 4 + ceil((product_length / 4) - (product_length // 4))
+    params = {'no of slides ' : no_of_slides,'range' : range(1,no_of_slides),'product':products}
+
+    return render(request,'shop/index.html',params)
+
 
 def about(request):
-    return render(request,"")
+    return render(request,"shop/about.html")
 
 def checkout(request):
     return render(request,"")
