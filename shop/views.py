5ff957f372cce9a98b043215cef843f8424b6d30
haripadhi08 <haripadhi08@gmail.com>
2023-04-16 10:54:04 +0530

    Added a category filter with each slider showing a particular category

--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from . models import Product
 from math import ceil
-# from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    product_length = len(products)
+    #products = Product.objects.all()
+   
+    all_products = []
+    product_category = Product.objects.values('category','product_id')
+    product_category_set = {item['category'] for item in product_category}
 
-    no_of_slides = product_length// 4 + ceil((product_length / 4) - (product_length // 4))
-    params = {'no of slides ' : no_of_slides,'range' : range(1,no_of_slides),'product':products}
+    for categorys in product_category_set:
+        products = Product.objects.filter(category = categorys)
+        product_length = len(products)
 
-    return render(request,'shop/index.html',params)
+        no_of_slides = product_length// 4 + ceil((product_length / 4) - (product_length // 4))
+        all_products.append([products,range(1,no_of_slides),no_of_slides])
+
+    new_params = {"all_products": all_products}
+
+    return render(request,'shop/index.html',new_params)
 
 
 def about(request):
